873ded24ec9106cafc040226d559b5d3c10ec561
shiftedchaos <71930716+shiftedchaos@users.noreply.github.com>
2020-09-26 20:18:21 +0300

    1

--- task5.cpp
+++ task5.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
+#include <cmath>
 
 int main()
 {
-  double a, b, c, d;
+  double a, b, c, d, x;
 
   std::cout << "Input the value of a: ";
   std::cin >> a;
@@ -12,13 +13,14 @@ int main()
   std::cin >> c;
   if (a == 0)
   {
-    std::cout << "Not a quadratic equation."
+    std::cout << "Not a quadratic equation.";
+    return 0;
   }
   d = b * b - 4 * a * c;
   if (d >= 0)
-  {
-    std
-  }
-
+    std::cout << "Roots: " << ((-b + sqrt(d)) / (2 * a)) << ", "
+     ((-b - sqrt(d)) / (2 * a));
+  else
+    std::cout << "No Real roots! Think about it, man!!!";
   return 0;
 }
