0ecf3462dbc3750765d97a6f7ee3d9f175b1ccdd
shiftedchaos <71930716+shiftedchaos@users.noreply.github.com>
2020-09-26 19:59:07 +0300

    1

---
+++ task5.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main()
+{
+  double a, b, c, d;
+
+  std::cout << "Input the value of a: ";
+  std::cin >> a;
+  std::cout << "Input the value of b: ";
+  std::cin >> b;
+  std::cout << "Input the value of c: ";
+  std::cin >> c;
+  if (a == 0)
+  {
+    std::cout << "Not a quadratic equation."
+  }
+  d = b * b - 4 * a * c;
+  if (d >= 0)
+  {
+    std
+  }
+
+  return 0;
+}
