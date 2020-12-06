3a96e4859b12e888bbbde710abb99e57a3f52e37
shiftedchaos <71930716+shiftedchaos@users.noreply.github.com>
2020-12-06 23:53:39 +0300

    part done

--- 5th task/task4.cpp
+++ 5th task/task4.cpp
@@ -16,6 +16,23 @@ int main()
   else
   {
     cout << "opened" << endl;
-  }
 
+    //int 2;
+    double arr[4];
+    for (size_t i = 0; i < 4; ++i)
+    {
+      fin >> arr[i];
+    }
+    for (size_t i = 0; i < 4; i++)
+    {
+      std::cout << arr[i] << ' ';
+    }
+    double r, i;
+    r = arr[0] + arr[2];
+    i = arr[1] + arr[4];
+    cout << endl << r << endl;
+    cout << i << endl;
+
+    }
+  return 0;
 }
