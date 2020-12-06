4e27c7a5a1f61fa1c4c19d1855e778f58545b517
shiftedchaos <71930716+shiftedchaos@users.noreply.github.com>
2020-12-07 00:38:20 +0300

    111

--- 5th task/task4.cpp
+++ 5th task/task4.cpp
@@ -9,16 +9,8 @@ int main()
   ifstream fin;
   fin.open(path);
 
-  if (!fin.is_open())
-  {
-    cout << "Error" << endl;
-  }
-  else
-  {
-    cout << "opened" << endl;
-
-    //int 2;
     double arr[4];
+    double r, i;
     for (size_t i = 0; i < 4; ++i)
     {
       fin >> arr[i];
@@ -27,12 +19,21 @@ int main()
     {
       std::cout << arr[i] << ' ';
     }
-    double r, i;
+
     r = arr[0] + arr[2];
-    i = arr[1] + arr[4];
+    i = arr[1] + arr[3];
     cout << endl << r << endl;
     cout << i << endl;
 
-    }
+  ofstream fout;
+
+  fout.open(path, ofstream::app);
+    fout << "\n";
+    fout << r << "\n";
+    fout << i;
+
+  fout.close();
+  fin.close();
+
   return 0;
 }
