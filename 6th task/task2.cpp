38c75874b3e5af1a913072d25e99db1c5784b57a
shiftedchaos <71930716+shiftedchaos@users.noreply.github.com>
2020-12-13 05:10:41 +0300

    class ArrayInt will be used in task3 (probably?)

--- 6th task/task2.cpp
+++ 6th task/task2.cpp
@@ -1,35 +1,29 @@
 #include <iostream>
-#include <cstdlib>
-#include <ctime>
 
-using namespace std;
+class ArrayInt
+{
+public:
+  int arr_len;
+  int *arr;
+  ArrayInt () //default constructor
+  {
+    int arr_len = 0;
+    int *arr = nullptr;
+  }
+  ArrayInt (int len) // constructor with 1 param.
+  {
+    arr_len = len; //инициализация длины массива переданным в конструктор значением
+    arr = new int[len]; //динамическое выделение памяти под массив соответствующей длины.
+  }
+  ~ArrayInt ()
+  {
+    delete[] arr;
+  }
+};
 
-double getRandom(double min, double max) {
-    double RandZero = rand() / static_cast<double>(RAND_MAX);
-    double RandInterval = RandZero * (max - min);
-    double RandMinMax = RandInterval + min;
-    return RandMinMax;
-}
+
+
 int main()
 {
-    double R;
-    double pi;
-    double n = 0, k = 0;
-    double x, y;
-    cin >> R;
-    cin >> n;
-    //x = new double[n];
-    //y = new double[n];
-    srand(time(0));
-    for (int i = 0; i < n; i++) {
-        x = getRandom(-R, R);
-        y = getRandom(-R, R);
-        //cout << x << ' ' << y << "\n";
-        if (x * x + y * y <= R * R)
-            k++;
-    }
-    //cout.precision(4);
-    cout << n << ' ' << k << endl;
-    pi = 4 * k / n;
-    cout << pi << endl;
+  return 0;
 }
