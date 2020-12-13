a3e5d0c1acae8a55390b1bd984b6e50d7d672ea9
shiftedchaos <71930716+shiftedchaos@users.noreply.github.com>
2020-12-13 06:32:20 +0300

    3

--- 6th task/task3.cpp
+++ 6th task/task3.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
+using namespace std;
 
 class ArrayInt
 {
-public:
+private:
   int arr_len;
   int *arr;
+public:
   ArrayInt () //default constructor
   {
     int arr_len = 0;
@@ -20,7 +22,9 @@ public:
     delete[] arr;
   }
 
-  void reallocate(int newlength)
+  void setValue(int index, int value) { arr[index] = value; }
+      int getValue(int index) { return arr[index]; }
+  void reallocate(int newlen)
   {
     delete[] arr; //удаление уже существующего массива
     int *newarr = new int[newlen]; //выделение памяти под массив новой длины
@@ -28,28 +32,32 @@ public:
 
   void insertBefore(int value, int index)
   {
-    int *newarr = new int[arr_len + 1];
-    for (size_t i = 0; i < index; i++)
+    int *newarr = new int[arr_len + 11]; //новый массив длины на 1 больше
+    for (size_t i = 0; i < index; i++) // скопировать в новый массив элементы из старого до index
     {
       newarr[i] = arr[i];
     }
-    newarr[index] = value;
-    for (size_t i = (index + 1); i < (arr_len + 1) ; i++)
+    newarr[index] = value; //вставить в новый массив value по индексу index
+    for (size_t i = (index + 1); i < (arr_len + 1) ; i++) //скопировать в новый массив элементы из старого от index до конца
     {
       newarr[i] = arr[i];
     }
-    *arr = *newarr;
     delete[] newarr;
+    *arr = *newarr; // удаляем старый массив и присваиваем указателю на старый массив указатель на новый массив
+    //delete[] newarr;
   }
 };
 
-int main()
-{
-ArrayInt arr
-
-
-
-
-
-  return 0;
+int main() {
+    ArrayInt arr(10);
+    for (int i = 0; i < 10; i++)
+        arr.setValue(i, i + 1);
+    for (int i = 0; i < 10; i++)
+        cout << arr.getValue(i) << ' ';
+    cout << endl;
+    arr.insertBefore(7, 3);
+    cout << "The value of element number 7 is " << arr.getValue(7) << endl;
+    for (int i = 0; i < 11; i++)
+        cout << arr.getValue(i) << ' ';
+    return 0;
 }
